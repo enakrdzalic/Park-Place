631d566b82b2c9bac839bd4044427b14fb73e940
Ena Krdzalic <enakrdzalic@gmail.com>
2015-06-12 14:32:15 -0700

    Added more markers

--- park_place/app/controllers/application_controller.rb
+++ park_place/app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+    def parse
+        require 'csv'
+        this_dir = File.dirname(__FILE__)
+        file_path = File.join(this_dir, 'lib', 'parks.csv')
+        
+        # Ans by Tom De Leu 2012
+        CSV.foreach(file_path, :headers => true) do |row|
+            
+            parkHasWashroom = false;
+            if row[14] = 'Y'
+                parkHasWashroom = true
+            end
+            
+            latlng = row[7].split(',')
+            
+            Park.create!(:name => row[1], :lat =>  latlng[0].to_f, :lng => latlng[1].to_f, :hasWashroom => parkHasWashroom)
+        end
+    end
 end
