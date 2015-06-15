8690f89c21925126d6e5803fff32364296447f74
Chris Hunter <christopherhunter@live.ca>
2015-06-15 14:19:05 -0700

    Updated Master

--- app/controllers/pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class PagesController < ApplicationController
-	def Map
-    end
-
-    def login
-    end
-end
--- app/models/park.rb
+++
@@ -1,3 +0,0 @@
-class Park < ActiveRecord::Base
-    # rails knows about all the things in the Park database columns
-end
