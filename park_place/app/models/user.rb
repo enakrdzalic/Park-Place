a87f38ca4ca2be82cc8ff342a0760d7a227b0ed5
Chris Hunter <christopherhunter@live.ca>
2015-07-09 18:25:36 -0700

    User interface updated
    Now has 4 functions for easy favourites.
    addFavourite    ** also checks for full favs
    removeFavourite
    has5Favourites
    getFavourites
    
    can all be accessed using dot notation in the User class.

--- park_place/app/models/user.rb
+++ park_place/app/models/user.rb
@@ -1,4 +1,40 @@
 class User < ActiveRecord::Base
     has_many :favourites
     has_many :parks, through:  :favourites
+    
+    
+    def addFavourite(p)
+
+        if (has5Favourites == false)
+            Favourite.create(:user_id => self.id, :park_id => p.parkID)
+            return true
+        end
+        
+        return false
+    end
+    
+    
+    
+    def removeFavourite(p)
+        Favourite.where(["user_id = ? and park_id = ?", self.id, p.parkID]).first.delete
+    end
+    
+    
+    
+    def has5Favourites
+        numFavs = Favourite.where("user_id = #{userID}").length
+        if numFavs >= 5
+            return true
+        end
+        
+        return false
+    end
+    
+    
+    
+    def getFavourites
+        userID = self.id
+        return Favourite.where("user_id = #{userID}").all
+    end
+    
 end
