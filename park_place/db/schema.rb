6533a44fc4e2e43257c9955c55c03332a580aec2
Ena Krdzalic <enakrdzalic@gmail.com>
2015-07-10 14:02:19 -0700

    Preparing for merge

--- park_place/db/schema.rb
+++ park_place/db/schema.rb
@@ -24,23 +24,23 @@ ActiveRecord::Schema.define(version: 20150708234456) do
   add_index "favourites", ["user_id"], name: "index_favourites_on_user_id", using: :btree
 
   create_table "parks", force: :cascade do |t|
-    t.integer  "parkID",        limit: 4
-    t.integer  "index",         limit: 4
-    t.string   "name",          limit: 255
-    t.float    "lat",           limit: 24
-    t.float    "lng",           limit: 24
-    t.string   "neighbourhood", limit: 255
-    t.boolean  "hasWashroom",   limit: 1,   default: false
-    t.boolean  "isLarge",       limit: 1,   default: false
+    t.string   "name",        limit: 255
+    t.float    "lat",         limit: 24
+    t.float    "lng",         limit: 24
+    t.boolean  "hasWashroom", limit: 1,   default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "index",       limit: 4
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "uid",        limit: 255
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "provider",         limit: 255
+    t.string   "uid",              limit: 255
+    t.string   "name",             limit: 255
+    t.string   "oauth_token",      limit: 255
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
 end
