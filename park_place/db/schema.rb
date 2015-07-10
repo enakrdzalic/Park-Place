26fba973d37b2a39ee6fca76008e243cf4132f26
Ena Krdzalic <enakrdzalic@gmail.com>
2015-07-10 15:50:30 -0700

    Fixed issue of table going below map sometimes

--- park_place/db/schema.rb
+++ park_place/db/schema.rb
@@ -24,12 +24,6 @@ ActiveRecord::Schema.define(version: 20150708234456) do
   add_index "favourites", ["user_id"], name: "index_favourites_on_user_id", using: :btree
 
   create_table "parks", force: :cascade do |t|
-<<<<<<< HEAD
-    t.string   "name",        limit: 255
-    t.float    "lat",         limit: 24
-    t.float    "lng",         limit: 24
-    t.boolean  "hasWashroom", limit: 1,   default: false
-=======
     t.integer  "parkID",        limit: 4
     t.integer  "index",         limit: 4
     t.string   "name",          limit: 255
@@ -38,20 +32,15 @@ ActiveRecord::Schema.define(version: 20150708234456) do
     t.string   "neighbourhood", limit: 255
     t.string   "hasWashroom",   limit: 255, default: "0"
     t.string   "isLarge",       limit: 255, default: "0"
->>>>>>> origin/ChRiSbRaNcH
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "index",       limit: 4
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider",         limit: 255
-    t.string   "uid",              limit: 255
-    t.string   "name",             limit: 255
-    t.string   "oauth_token",      limit: 255
-    t.datetime "oauth_expires_at"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.string   "uid",        limit: 255
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
