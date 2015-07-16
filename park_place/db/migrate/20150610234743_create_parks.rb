2206113465b70dfa709cfe946873224c92a5122a
Ena Krdzalic <enakrdzalic@gmail.com>
2015-07-15 21:01:04 -0700

    Refactored finished product

--- park_place/db/migrate/20150610234743_create_parks.rb
+++ park_place/db/migrate/20150610234743_create_parks.rb
@@ -1,14 +1,14 @@
 class CreateParks < ActiveRecord::Migration
   def change
     create_table :parks do |t|
-        t.integer "parkID"
-        t.integer "index"
-        t.string "name"
-        t.float "lat"
-        t.float "lng"
-        t.string "neighbourhood"
-        t.string "hasWashroom"
-        t.string "isLarge"
+        t.integer :parkID
+        t.integer :index
+        t.string :name
+        t.float :lat
+        t.float :lng
+        t.string :neighbourhood
+        t.string :hasWashroom
+        t.string :isLarge
         t.timestamps
     end
   end
--- park_place/db/migrate/20150708234456_favourites.rb
+++ park_place/db/migrate/20150708234456_favourites.rb
@@ -3,7 +3,7 @@ class Favourites < ActiveRecord::Migration
         create_table :favourites do |t|
             t.belongs_to :user, index: true
             t.belongs_to :park, index: true
-            t.string "uid"
+            t.string :uid
             t.timestamps null: false
         end
     end
